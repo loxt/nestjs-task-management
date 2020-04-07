81861b63ff6c7426301fc085523efb1884b292eb
loxt <emannuelloxt@hotmail.com>
2020-04-07 14:37:33 -0400

    feat: defining task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = ['task 1', 'task 2'];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
