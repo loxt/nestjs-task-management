1079d07164908bba128c5a79162a082c0d4bdfe1
loxt <emannuelloxt@hotmail.com>
2020-07-18 13:28:43 -0400

    :white_check_mark: test(userEntity): check password

--- src/auth/user.entity.spec.ts
+++ src/auth/user.entity.spec.ts
@@ -6,12 +6,27 @@ describe('UserEntity', () => {
 
   beforeEach(() => {
     user = new User();
+    user.password = 'testPassword';
     user.salt = 'testSalt';
     bcrypt.hash = jest.fn();
   });
 
   describe('validatePassword', () => {
-    it('should return true as password is valid', function () {});
-    it('should return false as password is invalid', function () {});
+    it('should return true as password is valid', async function () {
+      bcrypt.hash.mockReturnValue('testPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('123456');
+
+      expect(bcrypt.hash).toHaveBeenCalledWith('123456', 'testSalt');
+      expect(result).toEqual(true);
+    });
+    it('should return false as password is invalid', async function () {
+      bcrypt.hash.mockReturnValue('wrongPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('wrongPassword');
+
+      expect(bcrypt.hash).toHaveBeenCalledWith('wrongPassword', 'testSalt');
+      expect(result).toEqual(false);
+    });
   });
 });
